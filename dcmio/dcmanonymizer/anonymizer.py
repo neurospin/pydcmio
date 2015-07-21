cf574cd5997115dfde714c610d3a4734aeaf84a3
David Goyard <david.goyard@cea.fr>
2015-07-21 10:53:53 +0200

    dcmio: dcmanonymizer/anonymizer.py: adding header dcmreader/dcmreader.py: adding header

--- dcmio/dcmanonymizer/anonymizer.py
+++ dcmio/dcmanonymizer/anonymizer.py
@@ -1,3 +1,13 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+##########################################################################
+# NSAP - Copyright (C) CEA, 2013
+# Distributed under the terms of the CeCILL-B license, as published by
+# the CEA-CNRS-INRIA. Refer to the LICENSE file or to
+# http://www.cecill.info/licences/Licence_CeCILL-B_V1-en.html
+# for details.
+##########################################################################
+
 # anonymizer dicom
 import dicom
 import os
