6cb55e3f415ed0419e91c75082f70041076d40c0
Antoine Grigis <antoine.grigis@cea.fr>
2015-09-03 18:03:57 +0200

    dcmio/dcmconverter/dcm_to_nii: update to generate catalogue.

--- dcmio/test/test_dcm_to_nii.py
+++ dcmio/test/test_dcm_to_nii.py
@@ -8,63 +8,80 @@
 ##########################################################################
 
 # System import
-import os
-import sys
 import unittest
-import tempfile
-import shutil
 
-# Capsul import
-from capsul.study_config.study_config import StudyConfig
-from capsul.process.loader import get_process_instance
 
-# Mmutils import
-from mmutils.toy_datasets import get_sample_data
+def pilot_dcm2nii():
+    """ 
+    Dicom to nifti.
+
+    Imports
+    """
+    import os
+    import sys
+    import shutil
+    import tempfile
+    from capsul.study_config.study_config import StudyConfig
+    from capsul.process.loader import get_process_instance
+    from mmutils.toy_datasets import get_sample_data
+
+    """
+    Parameters
+    """
+    pipeline_name = "dcmio.dcmconverter.dcm_to_nii.xml"
+    outdir = tempfile.mkdtemp()
+
+    """
+    Configure the environment
+    """
+    study_config = StudyConfig(
+        modules=[],
+        use_smart_caching=True,
+        output_directory=self.outdir,
+        number_of_cpus=1,
+        generate_logging=True,
+        use_scheduler=True)
+
+    """
+    Create pipeline
+    """
+    pipeline = get_process_instance(self.pipeline_name)
+    pipeline.date_in_filename = True
+
+    """
+    Set pipeline input parameters
+    """
+    dicom_dataset = get_sample_data("dicom")
+    dcmfolder = os.path.join(self.outdir, "dicom")
+    if not os.path.isdir(dcmfolder):
+        os.makedirs(dcmfolder)
+    shutil.copy(dicom_dataset.barre, os.path.join(dcmfolder, "heart.dcm"))
+    pipeline.source_dir = dcmfolder
+
+    """
+    View pipeline
+    """
+    if 0:
+        from capsul.qt_gui.widgets import PipelineDevelopperView
+        from PySide import QtGui
+        app = QtGui.QApplication(sys.argv)
+        view1 = PipelineDevelopperView(pipeline)
+        view1.show()
+        app.exec_()
+
+    """
+    Execute the pipeline in the configured study
+    """
+    study_config.run(pipeline)
 
 
 class TestDcmToNii(unittest.TestCase):
     """ Class to test dicom to nifti pipeline.
     """
-    def setUp(self):
-        self.outdir = tempfile.mkdtemp()
-        # self.outdir = "/volatile/nsap/catalogue/dicom_convert/"
-        self.pipeline_name = "dcmio.dcmconverter.dcm_to_nii.xml"
-
     def test_simple_run(self):
         """ Method to test a simple 1 cpu call with the scheduler.
         """
-        # Configure the environment
-        study_config = StudyConfig(
-            modules=[],
-            use_smart_caching=True,
-            output_directory=self.outdir,
-            number_of_cpus=1,
-            generate_logging=True,
-            use_scheduler=True)
-
-        # Create pipeline
-        pipeline = get_process_instance(self.pipeline_name)
-        pipeline.date_in_filename = True
-
-        # Set pipeline input parameters
-        dicom_dataset = get_sample_data("dicom")
-        dcmfolder = os.path.join(self.outdir, "dicom")
-        if not os.path.isdir(dcmfolder):
-            os.makedirs(dcmfolder)
-        shutil.copy(dicom_dataset.barre, os.path.join(dcmfolder, "heart.dcm"))
-        pipeline.source_dir = dcmfolder
-
-        # View pipeline
-        if 0:
-            from capsul.qt_gui.widgets import PipelineDevelopperView
-            from PySide import QtGui
-            app = QtGui.QApplication(sys.argv)
-            view1 = PipelineDevelopperView(pipeline)
-            view1.show()
-            app.exec_()
-
-        # Execute the pipeline in the configured study
-        study_config.run(pipeline)
+        pilot_dcm2nii()
 
 
 def test():
